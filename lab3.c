2627a4bdf3d3402d199d81e0894d1b3d3d89a906
janessir <70764585+janessir@users.noreply.github.com>
2021-02-11 17:15:15 +0800

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -13,108 +13,108 @@ Q4: recursive fn hasGreatGrandchild(): prints vaue stored in all nodes of nin tr
 
 */
 
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-typedef struct _btnode{
-	int item;
-	struct _btnode *left;
-	struct _btnode *right;
+// typedef struct _btnode{
+// 	int item;
+// 	struct _btnode *left;
+// 	struct _btnode *right;
 
-} BTNode;
+// } BTNode;
 
-void mirrorTree(BTNode *node);
-void printSmallerValues(BTNode *node, int m);
-int smallestValue(BTNode *node);
-int hasGreatGrandchild(BTNode *node);
+// void mirrorTree(BTNode *node);
+// void printSmallerValues(BTNode *node, int m);
+// int smallestValue(BTNode *node);
+// int hasGreatGrandchild(BTNode *node);
 
-void printTree_InOrder(BTNode *node);
+// void printTree_InOrder(BTNode *node);
 
-int main(int argc, const char * argv[]){
+// int main(int argc, const char * argv[]){
 
-	int i;
-	BTNode *root, *root2;
-	BTNode btn[15];
+// 	int i;
+// 	BTNode *root, *root2;
+// 	BTNode btn[15];
 
-	// Create the tree in Q1
-	// Using manual dynamic allocation of memory for BTNodes
+// 	// Create the tree in Q1
+// 	// Using manual dynamic allocation of memory for BTNodes
 
-	root = malloc(sizeof(BTNode));
-	root->item = 4;
+// 	root = malloc(sizeof(BTNode));
+// 	root->item = 4;
 
-	root->left = malloc(sizeof(BTNode));
-	root->left->item = 5;
+// 	root->left = malloc(sizeof(BTNode));
+// 	root->left->item = 5;
 
-	root->right = malloc(sizeof(BTNode));
-	root->right->item = 2;
+// 	root->right = malloc(sizeof(BTNode));
+// 	root->right->item = 2;
 
-	root->left->left = NULL;
+// 	root->left->left = NULL;
 
-	root->left->right = malloc(sizeof(BTNode));
-	root->left->right->item = 1;
+// 	root->left->right = malloc(sizeof(BTNode));
+// 	root->left->right->item = 1;
 
-	root->left->right->left = NULL;
-	root->left->right->right = NULL;
+// 	root->left->right->left = NULL;
+// 	root->left->right->right = NULL;
 
-	root->right->left = malloc(sizeof(BTNode));
-	root->right->left->item = 3;
+// 	root->right->left = malloc(sizeof(BTNode));
+// 	root->right->left->item = 3;
 
-	root->right->right = malloc(sizeof(BTNode));
-	root->right->right->item = 1;
+// 	root->right->right = malloc(sizeof(BTNode));
+// 	root->right->right->item = 1;
 
-	root->right->left->left = NULL;
+// 	root->right->left->left = NULL;
 
-	root->right->left->right = NULL;
+// 	root->right->left->right = NULL;
 
-	root->right->right->left = NULL;
+// 	root->right->right->left = NULL;
 
-	root->right->right->right = NULL;
+// 	root->right->right->right = NULL;
 
-	printf("Question 1");
-	printf("\nOrinial Tree:\n");
-	printTree_InOrder(root);
-	printf("\nmirrorTree():\n");
-	mirrorTree(root);
-	printTree_InOrder(root);
-	printf("\n\n");
+// 	printf("Question 1");
+// 	printf("\nOrinial Tree:\n");
+// 	printTree_InOrder(root);
+// 	printf("\nmirrorTree():\n");
+// 	mirrorTree(root);
+// 	printTree_InOrder(root);
+// 	printf("\n\n");
 
-	//question 2
-	printf("Question 2");
-	printf("\nInput m for question 2: ");
-	scanf("%d", &i);
-	printf("the values smaller than %d are: ", i);
-	printSmallerValues(root, i);
-	printf("\n\n");
+// 	//question 2
+// 	printf("Question 2");
+// 	printf("\nInput m for question 2: ");
+// 	scanf("%d", &i);
+// 	printf("the values smaller than %d are: ", i);
+// 	printSmallerValues(root, i);
+// 	printf("\n\n");
 
-	//question 3
-	printf("Question 3");
-	printf("\nThe smallest value in the tree is: %d\n\n", smallestValue(root));
+// 	//question 3
+// 	printf("Question 3");
+// 	printf("\nThe smallest value in the tree is: %d\n\n", smallestValue(root));
 
-	//question 4
-	// Create a tree for Q4: Tall enough so some nodes have great-grandchildren
-	// Use array of BTNodes, create tree by linking nodes together
-	for (i = 0; i <= 6; i++){
-		btn[i].item = i;
-		btn[i].left = &(btn[i * 2 + 1]);
-		btn[i].right = &(btn[i * 2 + 2]);
-	}
+// 	//question 4
+// 	// Create a tree for Q4: Tall enough so some nodes have great-grandchildren
+// 	// Use array of BTNodes, create tree by linking nodes together
+// 	for (i = 0; i <= 6; i++){
+// 		btn[i].item = i;
+// 		btn[i].left = &(btn[i * 2 + 1]);
+// 		btn[i].right = &(btn[i * 2 + 2]);
+// 	}
 
-	for (i = 7; i <= 14; i++){
-		btn[i].item = i;
-		btn[i].left = NULL;
-		btn[i].right = NULL;
-	}
-	root2 = &btn[0];
+// 	for (i = 7; i <= 14; i++){
+// 		btn[i].item = i;
+// 		btn[i].left = NULL;
+// 		btn[i].right = NULL;
+// 	}
+// 	root2 = &btn[0];
 
-	printf("Question 4\n");
-	printf("The tree for question 4 visited by in-order is: ");
-	printTree_InOrder(root2);
-	printf("\nThe values stored in all nodes of the tree that have at least one great-grandchild are: {");
-	hasGreatGrandchild(root2);
-	printf("}\n");
+// 	printf("Question 4\n");
+// 	printf("The tree for question 4 visited by in-order is: ");
+// 	printTree_InOrder(root2);
+// 	printf("\nThe values stored in all nodes of the tree that have at least one great-grandchild are: {");
+// 	hasGreatGrandchild(root2);
+// 	printf("}\n");
 
-	return 0;
-}
+// 	return 0;
+// }
 
 void mirrorTree(BTNode *node){
 	//can do pre-order
