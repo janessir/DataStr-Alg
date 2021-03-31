ba12df3328c246dc5d7ce0aa5de8819cf6234c2d
janessir <70764585+janessir@users.noreply.github.com>
2021-03-31 21:47:11 +0800

    Update a6.c

--- a6.c
+++ a6.c
@@ -414,3 +414,139 @@ int HashDelete(int key, HashSlot hashTable[])
     }
     return  TABLESIZE+1;
 }
+
+
+
+// ----------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TABLESIZE 37
+#define PRIME     13
+
+enum Marker {EMPTY,USED};
+
+typedef struct _slot{
+    int key;
+    enum Marker indicator;
+    int next;
+} HashSlot;
+
+int HashInsert(int key, HashSlot hashTable[]);
+int HashFind(int key, HashSlot hashTable[]);
+
+int hash(int key)
+{
+    return (key % TABLESIZE);
+}
+
+int main()
+{
+    int opt;
+    int i;
+    int key;
+    int index;
+    HashSlot hashTable[TABLESIZE];
+
+    for(i=0;i<TABLESIZE;i++){
+        hashTable[i].next = -1;
+        hashTable[i].key = 0;
+        hashTable[i].indicator = EMPTY;
+    }
+
+    printf("============= Hash Table ============\n");
+    printf("|1. Insert a key to the hash table  |\n");
+    printf("|2. Search a key in the hash table  |\n");
+    printf("|3. Print the hash table            |\n");
+    printf("|4. Quit                            |\n");
+    printf("=====================================\n");
+
+    printf("Enter selection: ");
+    scanf("%d",&opt);
+    while(opt>=1 && opt <=3){
+        switch(opt){
+        case 1:
+            printf("Enter a key to be inserted:\n");
+            scanf("%d",&key);
+            index = HashInsert(key,hashTable);
+            if(index <0)
+                printf("Duplicate key\n");
+            else if(index < TABLESIZE)
+                printf("Insert %d at index %d\n",key, index);
+            else
+                printf("Table is full.\n");
+            break;
+        case 2:
+            printf("Enter a key for searching in the HashTable:\n");
+            scanf("%d",&key);
+            index = HashFind(key, hashTable);
+
+            if(index!=-1)
+                printf("%d is found at index %d.\n",key,index);
+            else
+                printf("%d is not found.\n",key);
+            break;
+
+        case 3:
+            printf("index:\t key \t next\n");
+            for(i=0;i<TABLESIZE;i++) printf("%d\t%d\t%d\n",i, hashTable[i].key,hashTable[i].next);
+            break;
+        }
+        printf("Enter selection: ");
+        scanf("%d",&opt);
+    }
+    return 0;
+}
+
+int HashInsert(int key, HashSlot hashTable[])
+{
+    int ploc=-100, temp, loc=hash(key);
+    
+    //else
+    for(int i=0;i<TABLESIZE;i++){
+        
+        //check for duplicates
+        if(hashTable[loc].indicator==USED && hashTable[loc].key==key)
+            return -1;
+        
+        //found empty slot
+        if(hashTable[loc].indicator==EMPTY){
+            hashTable[loc].key=key;
+            hashTable[loc].indicator=USED;
+            if(ploc!=-100)
+                hashTable[ploc].next=loc;
+            return loc;
+        }
+        
+        //else go next loc
+        
+        //get the first loc w/ next==-1 to change it ltr when found the index of insertion
+        if(ploc==-100 && hashTable[ploc].next==-1)
+            ploc=loc;
+        
+        if(hashTable[loc].next==-1){
+            loc+=1; //lin probing
+            if(loc==TABLESIZE)
+                loc=0;
+        }
+        else
+            loc=hashTable[loc].next; //go next
+        
+        
+    }
+    return TABLESIZE;
+}
+
+int HashFind(int key, HashSlot hashTable[])
+{
+ // Write your code here
+    return -1;
+}
+
+
+//for(i=0;i<TABLESIZE;i++){
+//    hashTable[i].next = -1;
+//    hashTable[i].key = 0;
+//    hashTable[i].indicator = EMPTY;
+//}
