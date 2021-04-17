095cd7cbafdcba3ea47a2263be0d87246d41702f
janessir <70764585+janessir@users.noreply.github.com>
2021-04-17 17:38:36 +0800

    Update a7_8.c

--- a7_8.c
+++ a7_8.c
@@ -437,276 +437,261 @@ int CC (Graph g)
 
 // ------------------------------------------------------------------------------------
 
-// question 3
+// // question 3
 
-#include <stdio.h>
-#include <stdlib.h>
 
-typedef struct _arraynode
-{
-    int *itemArray;
-    int sizeArray; //the size of a possible sequence
-    struct _arraynode *next;
-}ArrayNode;
+// #include <stdio.h>
+// #include <stdlib.h>
 
-typedef struct _arraylist{
-   int size; //the number of possible sequences
-   ArrayNode *head;
-} ArrayList;
+// typedef struct _arraynode
+// {
+//     int *itemArray;
+//     int sizeArray; //the size of a possible sequence
+//     struct _arraynode *next;
+// }ArrayNode;
 
-typedef struct _listnode
-{
-    int item;
-    struct _listnode *next;
-} ListNode;
+// typedef struct _arraylist{
+//    int size; //the number of possible sequences
+//    ArrayNode *head;
+// } ArrayList;
 
-typedef struct _linkedlist{
-   int sum;
-   int size;
-   ListNode *head;
-} LinkedList;
+// typedef struct _listnode
+// {
+//     int item;
+//     struct _listnode *next;
+// } ListNode;
 
-// You should not change the prototypes of these functions
-//////LinkedList///////////////////////////////////////////
-int insertNode(LinkedList *ll, int index, int coin);
-int removeNode(LinkedList *ll, int index);
-ListNode *findNode(LinkedList ll, int index);
-void removeAllItems(LinkedList *ll);
-///////////////////////////////////////////////////
+// typedef struct _linkedlist{
+//    int sum;
+//    int size;
+//    ListNode *head;
+// } LinkedList;
 
-void sumToC(LinkedList* ll, int C, ArrayList* al);
-void insertToArrList(LinkedList* ll, ArrayList* al);
+// // You should not change the prototypes of these functions
+// //////LinkedList///////////////////////////////////////////
+// int insertNode(LinkedList *ll, int index, int coin);
+// int removeNode(LinkedList *ll, int index);
+// ListNode *findNode(LinkedList ll, int index);
+// void removeAllItems(LinkedList *ll);
+// ///////////////////////////////////////////////////
 
-int main()
-{
-    int C;
-    printf("Enter a number:\n");
-    scanf("%d",&C);
+// void sumToC(LinkedList* ll, int C, ArrayList* al);
+// //void printresult(LinkedList *ll);
+// void insertToArrList(LinkedList* ll, ArrayList* al);
 
-    LinkedList ll;
-    ll.head = NULL;
-    ll.sum = 0;
-    ll.size = 0;
-    ArrayList al;
-    al.head = NULL;
-    al.size = 0;
+// int main()
+// {
+//     int C;
+//     printf("Enter a number:\n");
+//     scanf("%d",&C);
 
-    sumToC(&ll,C,&al);
-    ArrayNode* temp;
-    int i,j;
-    temp = al.head;
-    for(i=0;i<al.size;i++){
-        for(j=0;j<temp->sizeArray;j++)
-            printf(" %d ",temp->itemArray[j]);
-        printf("\n");
-        temp = temp->next;
-    }
+//     LinkedList ll;
+//     ll.head = NULL;
+//     ll.sum = 0;
+//     ll.size = 0;
+//     ArrayList al;
+//     al.head = NULL;
+//     al.size = 0;
 
-    return 0;
-}
+//     sumToC(&ll,C,&al);
+//     ArrayNode* temp;
+//     int i,j;
+//     temp = al.head;
+//     for(i=0;i<al.size;i++){
+//         for(j=0;j<temp->sizeArray;j++)
+//             printf(" %d ",temp->itemArray[j]);
+//         printf("\n");
+//         temp = temp->next;
+//     }
+
+//     return 0;
+// }
 
 void sumToC(LinkedList* ll, int C, ArrayList* al)
 {
-    int i,j,k;
-    ListNode *cur;
+    int s,l;
+    insertNode(ll,0,1);
     
-    
-    for(i=1;i<=C;i++){
-        
-        removeAllItems(ll);
-        
-        //creating first listnode
-        insertNode(ll,0,i);
-        
-        ll->sum=i;
-        
-        //ie the last element == C
-        if(ll->sum==C){
+    //while ll is not empty
+    while(ll->size!=0){
+        if(ll->head->item==C){
+            //insert to array
             insertToArrList(ll,al);
-            return;
+//            printresult(ll);
+            break;
         }
-        
-        //check
-        j=i+1;
-        k=i+1;
-        while(k!=C){
-            
-            if(k==C-1)
-                j++;
-            
-            insertNode(ll,ll->size,k++);
-            
-            //found a permutation
-            if(ll->sum==C){
-                insertToArrList(ll,al);
-                
-                //backtrack by 2 steps to search rest
-                removeNode(ll,ll->size-1);
-                removeNode(ll,ll->size-1);
-                k=j+1;
-                j++;
-                if(k==C-1)
-                    break;
-            }
-            
-            else if(ll->sum<C){
-                continue;
-            }
-                
-            //ll->size>C
-            else{
-                //backtrack by 2 steps
-                removeNode(ll,ll->size-1);
-                removeNode(ll,ll->size-1);
-                k-=1;
-            }
+        if(ll->sum==C){
+            //insert to array
+            insertToArrList(ll,al);
+//            printresult(ll);
+            removeNode(ll,ll->size-1);
+            s=findNode(*ll,ll->size-1)->item;
+            removeNode(ll,ll->size-1);
+            insertNode(ll,ll->size,s+1);
+        }
+        else if(ll->sum<C){
+            l=findNode(*ll,ll->size-1)->item;
+            insertNode(ll,ll->size,l+1);
+        }
+        else{
+            removeNode(ll,ll->size-1);
+            s=findNode(*ll,ll->size-1)->item;
+            removeNode(ll,ll->size-1);
+            insertNode(ll,ll->size,s+1);
         }
     }
 }
 
+//void printresult(LinkedList *ll){
+//    //check
+//    ListNode *cur=ll->head;
+//    if(ll->size!=0){
+//        for (int j=0;j<ll->size;j++){
+//            printf("%d ",cur->item);
+//            cur=cur->next;
+//        }
+//        printf("\n");
+//    }
+//}
+
+
 void insertToArrList(LinkedList* ll, ArrayList* al){
-   
-    int i=0;
+    int i;
+    ArrayNode *temp;
     ListNode *cur=ll->head;
-    ArrayNode *tempAn;
-    
-    //first seq found
-    if(al->size==0){
-        al->head=malloc(sizeof(ArrayNode));
+    if (al->head == NULL){
+        al->head = (ArrayNode *)malloc(sizeof(ArrayNode));
         al->head->itemArray=malloc(ll->size*sizeof(int));
-        while(cur!=NULL){
-            al->head->itemArray[i++]=cur->item;
+        for(i=0;i<ll->size;i++){
+            al->head->itemArray[i]=cur->item;
             cur=cur->next;
         }
-        al->head->sizeArray=ll->size;
-        al->head->next=NULL;
+        al->head->sizeArray = ll->size;
+        al->head->next = NULL;
     }
-    
-    //subsequent seq
     else{
-        tempAn=al->head;
-        while(tempAn->next!=NULL)
-            tempAn=tempAn->next;
-        
-        tempAn->next=malloc(sizeof(ArrayNode*));
-        tempAn->next->itemArray=malloc(ll->size*sizeof(int));
-        while(cur!=NULL){
-            tempAn->next->itemArray[i++]=cur->item;
-            cur=cur->next;
+        temp = al->head;
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = (ArrayNode *)malloc(sizeof(ArrayNode));
+        temp->next->itemArray=malloc(ll->size*sizeof(int));
+        for(i=0;i<ll->size;i++){
+                temp->next->itemArray[i]=cur->item;
+                cur=cur->next;
         }
-        tempAn->next->sizeArray=ll->size;
-        tempAn->next->next=NULL;
+        temp->next->sizeArray = ll->size;
+        temp->next->next = NULL;
     }
-    
     al->size++;
 }
 
-///////////////////////////////////////////////////////
-int insertNode(LinkedList *ll, int index, int value){
-
-    ListNode *pre, *cur;
-
-    if (ll == NULL || index < 0 || index > ll->size)
-        return 0;
-
-        if (index == 0){
-        cur = ll->head;
-        ll->head = (ListNode*) malloc(sizeof(ListNode));
-        ll->head->item = value;
-        ll->head->next = cur;
-        ll->sum += value;
-        ll->size++;
-        return 1;
-    }
 
 
-    // Find the nodes before and at the target position
-    // Create a new node and reconnect the links
-    if ((pre = findNode(*ll, index - 1)) != NULL){
-        cur = pre->next;
-        pre->next = malloc(sizeof(ListNode));
-        pre->next->item = value;
-        pre->next->next = cur;
-        ll->sum += value;
-        ll->size++;
-        return 1;
-    }
+// ///////////////////////////////////////////////////////
+// int insertNode(LinkedList *ll, int index, int value){
 
-    return 0;
-}
+//     ListNode *pre, *cur;
 
+//     if (ll == NULL || index < 0 || index > ll->size)
+//         return 0;
 
-int removeNode(LinkedList *ll, int index){
-
-    ListNode *pre, *cur;
-
-    // Highest index we can remove is size-1
-    if (ll == NULL || index < 0 || index > ll->size)
-        return 0;
-
-    // If removing first node, need to update head pointer
-    if (index == 0){
-        cur = ll->head->next;
-        ll->sum -= ll->head->item;
-        free(ll->head);
-        ll->head = cur;
-        ll->size--;
-        return 1;
-    }
-
-    // Find the nodes before and after the target position
-    // Free the target node and reconnect the links
-    if ((pre = findNode(*ll, index - 1)) != NULL){
-
-        if (pre->next == NULL)
-            return 0;
-
-        cur = pre->next;
-        ll->sum -= cur->item;
-        pre->next = cur->next;
-        free(cur);
-        ll->size--;
-        return 1;
-    }
-
-    return 0;
-}
-
-ListNode *findNode(LinkedList ll, int index){
-
-    ListNode *temp;
-
-    if (index < 0 || index >= ll.size)
-        return NULL;
-
-    temp = ll.head;
-
-    if (temp == NULL || index < 0)
-        return NULL;
-
-    while (index > 0){
-        temp = temp->next;
-        if (temp == NULL)
-            return NULL;
-        index--;
-    }
-
-    return temp;
-}
-
-void removeAllItems(LinkedList *ll)
-{
-    ListNode *cur = ll->head;
-    ListNode *tmp;
-
-    while (cur != NULL){
-        tmp = cur->next;
-        free(cur);
-        cur = tmp;
-    }
-    ll->head = NULL;
-    ll->size = 0;
-    ll->sum =0;
-}
+//         if (index == 0){
+//         cur = ll->head;
+//         ll->head = (ListNode*) malloc(sizeof(ListNode));
+//         ll->head->item = value;
+//         ll->head->next = cur;
+//         ll->sum += value;
+//         ll->size++;
+//         return 1;
+//     }
 
 
+//     // Find the nodes before and at the target position
+//     // Create a new node and reconnect the links
+//     if ((pre = findNode(*ll, index - 1)) != NULL){
+//         cur = pre->next;
+//         pre->next = malloc(sizeof(ListNode));
+//         pre->next->item = value;
+//         pre->next->next = cur;
+//         ll->sum += value;
+//         ll->size++;
+//         return 1;
+//     }
+
+//     return 0;
+// }
+
+
+
+// int removeNode(LinkedList *ll, int index){
+
+//     ListNode *pre, *cur;
+
+//     // Highest index we can remove is size-1
+//     if (ll == NULL || index < 0 || index > ll->size)
+//         return 0;
+
+//     // If removing first node, need to update head pointer
+//     if (index == 0){
+//         cur = ll->head->next;
+//         ll->sum -= ll->head->item;
+//         free(ll->head);
+//         ll->head = cur;
+//         ll->size--;
+//         return 1;
+//     }
+
+//     // Find the nodes before and after the target position
+//     // Free the target node and reconnect the links
+//     if ((pre = findNode(*ll, index - 1)) != NULL){
+
+//         if (pre->next == NULL)
+//             return 0;
+
+//         cur = pre->next;
+//         ll->sum -= cur->item;
+//         pre->next = cur->next;
+//         free(cur);
+//         ll->size--;
+//         return 1;
+//     }
+
+//     return 0;
+// }
+
+// ListNode *findNode(LinkedList ll, int index){
+
+//     ListNode *temp;
+
+//     if (index < 0 || index >= ll.size)
+//         return NULL;
+
+//     temp = ll.head;
+
+//     if (temp == NULL || index < 0)
+//         return NULL;
+
+//     while (index > 0){
+//         temp = temp->next;
+//         if (temp == NULL)
+//             return NULL;
+//         index--;
+//     }
+
+//     return temp;
+// }
+
+// void removeAllItems(LinkedList *ll)
+// {
+//     ListNode *cur = ll->head;
+//     ListNode *tmp;
+
+//     while (cur != NULL){
+//         tmp = cur->next;
+//         free(cur);
+//         cur = tmp;
+//     }
+//     ll->head = NULL;
+//     ll->size = 0;
+//     ll->sum =0;
+// }
