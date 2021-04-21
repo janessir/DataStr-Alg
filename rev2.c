ca0ee02443e30714b14a7572f3ec49ac63edf38d
janessir <70764585+janessir@users.noreply.github.com>
2021-04-21 17:34:59 +0800

    Update rev2.c

--- rev2.c
+++ rev2.c
@@ -1,43 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+// lab1 q1
 
-#define SIZE 80 //The limit of expression length
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <ctype.h>
 
-typedef struct _stackNode{
-    char item;
-    struct _stackNode *next;
-}StackNode;
+// #define SIZE 80 //The limit of expression length
 
-typedef struct _stack{
-   int size;
-   StackNode *head;
-}Stack;
+// typedef struct _stackNode{
+//     char item;
+//     struct _stackNode *next;
+// }StackNode;
 
-void push(Stack *sPtr, char item);
-int pop(Stack *sPtr);
-char peek(Stack s);
-int isEmptyStack(Stack s);
+// typedef struct _stack{
+//    int size;
+//    StackNode *head;
+// }Stack;
 
-void in2Post(char*, char*);
-int highEqPrec(char sOp, char cOp);
+// void push(Stack *sPtr, char item);
+// int pop(Stack *sPtr);
+// char peek(Stack s);
+// int isEmptyStack(Stack s);
 
-int main()
-{
-    char infix[SIZE];
-    char postfix[SIZE];
+// void in2Post(char*, char*);
+// int highEqPrec(char sOp, char cOp);
 
-    printf("Enter an infix expression:\n");
-    fgets(infix,SIZE,stdin);
-    char* p=strchr(infix,'\n');
-    *p='\0';
+// int main()
+// {
+//     char infix[SIZE];
+//     char postfix[SIZE];
 
-    in2Post(infix,postfix);
-    printf("The postfix expression is \n");
-    printf("%s\n",postfix);
-    return 0;
-}
+//     printf("Enter an infix expression:\n");
+//     fgets(infix,SIZE,stdin);
+//     char* p=strchr(infix,'\n');
+//     *p='\0';
+
+//     in2Post(infix,postfix);
+//     printf("The postfix expression is \n");
+//     printf("%s\n",postfix);
+//     return 0;
+// }
 
 //infix to postfix
 void in2Post(char* infix, char* postfix)
@@ -100,33 +102,37 @@ int highEqPrec(char sOp, char cOp){
     return 0;
 }
 
-void push(Stack *sPtr, char item){
-    StackNode *newNode;
-    newNode = malloc(sizeof(StackNode));
-    newNode->item = item;
-    newNode->next = sPtr->head;
-    sPtr->head = newNode;
-    sPtr->size++;
-}
+// void push(Stack *sPtr, char item){
+//     StackNode *newNode;
+//     newNode = malloc(sizeof(StackNode));
+//     newNode->item = item;
+//     newNode->next = sPtr->head;
+//     sPtr->head = newNode;
+//     sPtr->size++;
+// }
 
-int pop(Stack *sPtr){
-    if(sPtr == NULL || sPtr->head == NULL){
-        return 0;
-    }
-    else{
-       StackNode *temp = sPtr->head;
-       sPtr->head = sPtr->head->next;
-       free(temp);
-       sPtr->size--;
-       return 1;
-    }
-}
+// int pop(Stack *sPtr){
+//     if(sPtr == NULL || sPtr->head == NULL){
+//         return 0;
+//     }
+//     else{
+//        StackNode *temp = sPtr->head;
+//        sPtr->head = sPtr->head->next;
+//        free(temp);
+//        sPtr->size--;
+//        return 1;
+//     }
+// }
 
-char peek(Stack s){
-    return s.head->item;
-}
+// char peek(Stack s){
+//     return s.head->item;
+// }
 
-int isEmptyStack(Stack s){
-     if(s.size == 0) return 1;
-     else return 0;
-}
+// int isEmptyStack(Stack s){
+//      if(s.size == 0) return 1;
+//      else return 0;
+// }
+
+// ---------------------------------------------------------------------------
+
+
